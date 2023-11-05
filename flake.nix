212308e35838a9027e35964f63cb68de9055e1a1
Jacek Galowicz <jacek@galowicz.de>
2023-11-05 15:06:17 +0100

    Add `nix run` script

--- flake.nix
+++ flake.nix
@@ -7,13 +7,31 @@
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    perSystem = { config, self', inputs', pkgs, system, ... }: {
+    perSystem = { config, pkgs, ... }: {
       packages = {
+        default = config.packages.nixery;
+
         nixery = pkgs.callPackage ./package.nix {
           inherit (config.packages) nixery-prepare-image;
         };
         nixery-prepare-image = pkgs.callPackage ./prepare-image { };
         nixery-popcount = pkgs.callPackage ./popcount { };
+      };
+
+      apps.default = {
+        type = "app";
+        program =
+          let
+            env = { nativeBuildInputs = [ pkgs.makeWrapper ]; };
+            app = pkgs.runCommand "nixery-server" env ''
+              makeWrapper ${config.packages.nixery}/bin/server $out/bin/nixery-server \
+                --set-default PORT 8000 \
+                --set-default NIXERY_PKGS_PATH "${pkgs.path}" \
+                --set-default NIXERY_STORAGE_BACKEND "filesystem" \
+                --set-default STORAGE_PATH "./." \
+            '';
+          in
+            "${app}/bin/nixery-server";
       };
 
       devShells.default = pkgs.mkShell {
